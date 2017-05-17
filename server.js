dde65ba3e51456ef96c5300d8a900e1c7777d516
Mario Vizcaino <mario.vizcainos@gmail.com>
2017-05-17 03:16:58 +0000

    added logic in the app and cors

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var api = require('timestamp.js');
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+    
+var port = process.env.PORT || 8080;        // set our port
+    
+api(app);
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+});
